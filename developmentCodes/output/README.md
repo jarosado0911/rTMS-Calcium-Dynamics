ee9fbf6ba5074deaa2c254d3951b35df00e65982
James Rosado <jarosado0911@gmail.com>
2021-02-26 18:12:23 -0500

    added added shell script for testing er channels on

---
+++ developmentCodes/output/README.md
@@ -0,0 +1,1 @@
+this is where the output data goes
---
+++ developmentCodes/runRTMS_Calcium.sh
@@ -0,0 +1,1 @@
+ugshell.exe -ex vdccFullCellCalcium.lua -grid sample_neuron6.swc -numRefs 0 -setting all -dt 1.0e-5 -endTime 1.0e-4 -vtk -pstep 1.0e-5 -vmData sample_vmData6 -outName output -solver GS -minDef 1e-11
