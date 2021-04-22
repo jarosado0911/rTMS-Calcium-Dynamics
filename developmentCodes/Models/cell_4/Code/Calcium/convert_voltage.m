4d489aab306fb5a440df57de140062f26b1ffe84
James Rosado <jarosado0911@gmail.com>
2021-04-22 16:47:59 -0400

    updated gui

--- developmentCodes/Models/cell_4/Code/Calcium/convert_voltage.m
+++ developmentCodes/Models/cell_4/Code/Calcium/convert_voltage.m
@@ -1,10 +1,25 @@
+function incomplete = convert_voltage()
 %% Read files
 locs = load(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'locs' filesep 'locs_all_seg.txt']);
-voltage = load(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'voltage_trace.dat'])';
+try
+    voltage = load(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'voltage_trace.dat'])';
+    tvec = load(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'tvec.dat']);
+catch
+    incomplete = 1;
+    return
+end
+%% Check
+if size(voltage,2) ~= length(tvec)
+    size(voltage)
+    incomplete = 1;
+    return
+end
 %% Parse
 output_folder = ['..' filesep '..' filesep 'Results' filesep ...
     'Calcium' filesep 'Converted_Voltage_Traces' filesep];
 for time_step = 1:size(voltage,2)
     output = [locs(:,1:3) voltage(:,time_step)];
     writematrix(output,[output_folder 'vm_' sprintf('%07d', time_step-1) '.dat'], 'Delimiter', 'space');
+end
+incomplete = 0;
 end
--- developmentCodes/Models/cell_4/Code/Calcium/export_data.m
+++ developmentCodes/Models/cell_4/Code/Calcium/export_data.m
@@ -1,8 +1,8 @@
-function export_data()
+function status = export_data()
 % Export the SWC file, and the voltage data in the format compatible with
 % the calcium concentration simulations.
-% 'neuron_out.swc' file containing the neuron morphology is placed in the results folder
-% Voltage data are placed in results\voltage_data_calcium
+% Results data are placed in Results\Calcium
+% Returns the status: missing input files = 0, incomplete voltage = 1, successful = 2
 %% Create output folder
 output_folder = ['..' filesep '..' filesep 'Results' filesep ...
     'Calcium' filesep 'Converted_Voltage_Traces'];
@@ -10,11 +10,23 @@ if(~exist(output_folder,'dir'))
     mkdir(output_folder); % create output directory
 end
 %% Generate SWC file
-generate_SWC;
+if exist(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'locs' filesep 'locs_all_seg.txt'])
+    generate_SWC;
+else
+    status = 0;
+    return
+end
 %% Generate voltage data in the correct format
-convert_voltage;
+if ~exist(['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'voltage_trace.dat'])
+    status = 0;
+    return
+elseif convert_voltage()
+    status = 1;
+    return
+end
 %% Copy the time vector
 t_in = ['..' filesep '..' filesep 'Results' filesep 'NEURON' filesep 'tvec.dat'];
 t_out = ['..' filesep '..' filesep 'Results' filesep 'Calcium' filesep];
 copyfile(t_in, t_out, 'f');
+status = 2;
 end
